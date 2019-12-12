3fcca8fa3c8d73a8dd2958edaea0ef35e18d63dc
Jose Casado <jcasado6@yahoo.com>
2019-12-12 17:21:50 -0500

    api pic rendering

--- src/App.js
+++ src/App.js
@@ -1,26 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from "react";
 import './App.css';
+import Square from './Square'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor (){
+    super ()
+    this.state = {
+      picture: []
+    }
+  }
+  componentDidMount(){
+    fetch ('https://api.nasa.gov/planetary/apod?api_key=ArL5YQaUhO73pDtc8SF1f4ZVgMUd2nlvRaOitLHQ')
+    .then(res => res.json())
+    .then(res => {
+      console.log(res)
+      this.setState({picture: res})
+      console.log(this.state.picture.hdurl)
+    })
+  }
+  render() {
+    let newPic = this.state.picture.map(picture => {
+      return <Square key={picture.hdurl} name={picture.hdurl}/>
+    })
+    return (
+      <div className="App">
+        <h1>Picture of the day</h1>
+        <img src={newPic} alt='pic'>  
+        </img>
+      </div>
+    );
+  }
 }
 
 export default App;
